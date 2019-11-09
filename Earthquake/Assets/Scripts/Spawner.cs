57d96a9feefc9ab7a2b645532ccfad0dd754b344
Joel Lajoie-Corriveau <53316867+VoltDriver@users.noreply.github.com>
2019-11-09 15:53:11 -0500

    Fixing Folders again + Adding car spawner basics
    UNFINISHED Car spawner, and fixing compile errors for the empty scripts.

---
+++ Earthquake/Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int m_maxCars = 5;
+    public int m_curCars = 0;
+    public GameObject m_car;
+
+    public int m_spawnRate = 5;
+    private int m_spawnTick = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_spawnTick++;
+
+        if(m_spawnTick == m_spawnRate)
+        {
+            // Check if we already have the maximum amount of cars in the game
+            if(m_curCars < m_maxCars)
+            {
+                // Spawn a car
+                GameObject newCar = Instantiate(m_car);
+                var position = this.transform.position;
+                newCar.transform.position = new Vector3(position.x + 3, position.y);
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
