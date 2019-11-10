3823061a173b3a4a25235c74df66fbec4302f2f8
Joel Lajoie-Corriveau <53316867+VoltDriver@users.noreply.github.com>
2019-11-10 14:15:39 -0500

    Scoreboard Added
    Added a basic scoreboard menu.

---
+++ Earthquake/Assets/Scripts/ScoreSave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScoreSave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Save()
+    {
+        string path = Path.m_savePath;
+
+        Debug.Log(path);
+
+        FileInfo fi = new FileInfo(path);
+        if (!fi.Directory.Exists)
+        {
+            System.IO.Directory.CreateDirectory(fi.DirectoryName);
+        }
+
+        using (StreamWriter writer = new StreamWriter(path) )
+        {
+            writer.WriteLine("JAJAJA|500");
+        }
+    }
+}
