442f409102972841ba993a4d6b9b366a843e4e19
Joel Lajoie-Corriveau <53316867+VoltDriver@users.noreply.github.com>
2019-11-10 15:51:07 -0500

    Scoreboard Work
    The Scoreboard now properly displays sorted scores

---
+++ Earthquake/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore
+{
+    public string Name { get; set; }
+    public uint Score { get; set; }
+}
---
+++ Earthquake/Assets/Scripts/PlayerScoreComparer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreComparer : IComparer<PlayerScore>
+{
+    public int Compare(PlayerScore x, PlayerScore y)
+    {
+        return x.Score.CompareTo(y.Score);
+    }
+}
