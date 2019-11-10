195e2205c16c41f47f9a9129741e0abf960f0121
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2019-11-10 13:14:24 -0500

    Functional NextLevel Screen

---
+++ Earthquake/Assets/Scripts/NextLevelScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextLevelScore : MonoBehaviour
+{
+    public Text scoretext = null;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Nextlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name + 1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        scoretext.text = Scorescript.Instance.GetScore().ToString();
+    }
+
+
+}
