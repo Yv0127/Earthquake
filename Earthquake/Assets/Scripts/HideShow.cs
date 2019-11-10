902071c6df78795ad9d423f0a926d7c15b094026
Joel Lajoie-Corriveau <53316867+VoltDriver@users.noreply.github.com>
2019-11-10 10:57:46 -0500

    Functioning Help Panel
    The Help button now show and hides the Help panel, and there is no weird text over the help button.
    
    Added a script to dynamically change the text of a component.

---
+++ Earthquake/Assets/Scripts/HideShow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShow : MonoBehaviour
+{
+    private bool m_isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleShow()
+    {
+        m_isActive = !m_isActive;
+
+        this.gameObject.SetActive(m_isActive);
+    }
+}
---
+++ Earthquake/Assets/Scripts/ShowPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPanel : MonoBehaviour
+{
+    GameObject m_panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
