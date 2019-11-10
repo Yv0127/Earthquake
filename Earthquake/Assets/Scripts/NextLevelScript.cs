d2c490c916a4cd3800c3ae39e7e290eea099f390
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2019-11-10 12:02:02 -0500

    NewScript

---
+++ Earthquake/Assets/Scripts/NextLevelScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI
+
+public class NextLevelScript : MonoBehaviour
+{
+    public Text scoretext = null
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name +1);
+    }
+
+    void quit()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        scoretext.text = Int.ToString(Scorescript.GetScore());
+    }
+}
